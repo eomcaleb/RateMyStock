088b9286dcecf455b99ccdc31f6b1de0a368234e
Caleb Eom <eomcaleb@gmail.com>
2022-02-12 14:20:01 -0500

    css change

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const app = Vue.createApp({
     data() {
         return {
-            BTCUSD: '',
-            BTCJPY: '',
-            BTCCAD: '',
-            ETHUSD: '',
-            ETHJPY: '',
-            ETHCAD: ''
+            BTCUSD: '-',
+            BTCEUR: '-',
+            BTCCAD: '-',
+            ETHUSD: '-',
+            ETHEUR: '-',
+            ETHCAD: '-'
         }
     },
 
     methods: {
         formatPrice(value) {
+            if (value == '-') return value
             let val = (value/1).toFixed(2).replace('.', ',')
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
         },
 
         async getBitcoinPrice() {
-            const result = await fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,CAD&api=726759b7ea67c89d05467916a56440c04b5ac563d9bdd48639fbd772503f6d04')
+            const result = await fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,CAD&api=726759b7ea67c89d05467916a56440c04b5ac563d9bdd48639fbd772503f6d04')
             const results = await result.json()
             this.BTCUSD = results.USD,
-            this.BTCJPY = results.JPY,
+            this.BTCEUR = results.EUR,
             this.BTCCAD = results.CAD
         },
 
         async getEthereumPrice() {
-            const result = await fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,JPY,CAD&api=726759b7ea67c89d05467916a56440c04b5ac563d9bdd48639fbd772503f6d04')
+            const result = await fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,CAD&api=726759b7ea67c89d05467916a56440c04b5ac563d9bdd48639fbd772503f6d04')
             const results = await result.json()
             this.ETHUSD = results.USD,
-            this.ETHJPY = results.JPY,
+            this.ETHEUR = results.EUR,
             this.ETHCAD = results.CAD
         }
     }
